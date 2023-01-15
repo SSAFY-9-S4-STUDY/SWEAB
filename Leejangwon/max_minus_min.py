942408ecf0600e777ec67bbb4a3665e167a8a609
wony.dev <programmerwony@gmail.com>
2023-01-15 16:46:46 +0900

    day1 max_minus_min added

---
+++ Leejangwon/max_minus_min.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+c = str(input())
+d = int(input())
+e = str(input())
+f = int(input())
+g = str(input())
+
+cl = list(map(int, c.split()))
+el = list(map(int, e.split()))
+gl = list(map(int, g.split()))
+
+print('#1' + ' ' + str(max(cl) - min(cl)))
+print('#2' + ' ' + str(max(el) - min(el)))
+print('#3' + ' ' + str(max(gl) - min(gl)))
