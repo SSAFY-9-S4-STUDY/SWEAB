bdd011e4010263e39c2ea873e1bc47a9661307c2
Koneweekk <rlagkswn37@naver.com>
2023-01-19 15:29:21 +0900

    modify some code

--- Kimhanju/0119/1110boj.py
+++ Kimhanju/0119/1110boj.py
@@ -1,8 +1,8 @@
 N = int(input())
-new_N,n = N,0
+target,cnt = N,0
 
-while new_N != N or n == 0:
+while target != N or cnt == 0:
     N = (N % 10)*10 + (N//10 + N%10) % 10
-    n += 1
+    cnt += 1
 
-print(n)
+print(cnt)
