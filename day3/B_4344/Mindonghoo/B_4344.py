75c76a258096682b87c407257f58ae557c1f5b64
Shiny-Gengar <redrg69@gmail.com>
2023-01-21 19:59:25 +0900

    donghoo submit B_4344

--- day3/B_4344/Mindonghoo/B_4344.py
+++ day3/B_4344/Mindonghoo/B_4344.py
@@ -4,7 +4,6 @@ for i in range(0, trial) :
     stest = list(map(int, input().split()))
     over_average = []
     average = sum(stest[1:len(stest)]) / stest[0]
-    print(average)
     for j in stest[1:len(stest)]:
         if j > average:
             over_average.append(j)
