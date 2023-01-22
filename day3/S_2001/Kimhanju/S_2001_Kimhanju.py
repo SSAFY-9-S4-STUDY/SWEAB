b992d2477213c17e83b440a794d4b75e17345675
Koneweekk <rlagkswn37@naver.com>
2023-01-22 15:04:41 +0900

    add day3 S_2001

---
+++ day3/S_2001/Kimhanju/S_2001_Kimhanju.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open('2001.txt','r')
+
+T = int(input())
+for test_case in range(1, T + 1):
+
+    N,M = map(int,input().split())
+
+    spaces = []
+    for _ in range(N):
+        spaces.append(list(map(int,input().split())))
+    
+    flys = []
+    for i in range(N-M+1):
+        for j in range(N-M+1):
+            sum_fly = 0
+            for m in range(M):
+                sum_fly += sum(spaces[i+m][j:j+M])
+            flys.append(sum_fly)
+    print(f'#{test_case} {max(flys)}')
