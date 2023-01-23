7800da67edbe103a2fad356845496d5364e0ea32
Kghyeong <abcd5ksw@hanmail.net>
2023-01-23 13:58:26 +0900

    modify 1065

--- day3/B_1065/B_1065_Kimgwanhyeong.py
+++ day3/B_1065/B_1065_Kimgwanhyeong.py
@@ -1,15 +1,18 @@
-num = int(input())
-if num < 100:
-    print(num)
-elif num <= 1000:
-    count = 99
-    for i in range(100,num+1):
-        first = i // 100
-        second = (i // 10) % 10
-        last = i % 10
-        if second - first == last - second:
-            count += 1
-        elif first - second == second - last:
-            count += 1
 
-    print(count)
+def hansu(num):
+    if num < 100:
+        return num
+    elif num <= 1000:
+        count = 99
+        for i in range(100,num+1):
+            first = i // 100
+            second = (i // 10) % 10
+            last = i % 10
+            if second - first == last - second:
+                count += 1
+            elif first - second == second - last:
+                count += 1
+        return count
+
+print(hansu(110))
+print(hansu(500))
