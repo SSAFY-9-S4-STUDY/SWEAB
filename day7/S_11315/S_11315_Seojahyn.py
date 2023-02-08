fee795ec188ddb209ee5d1dcca2f26c485865112
Sseo Jjam <124118518+sjh960815@users.noreply.github.com>
2023-02-08 20:46:14 +0900

    Update S_11315_Seojahyn.py

--- day7/S_11315/S_11315_Seojahyn.py
+++ day7/S_11315/S_11315_Seojahyn.py
@@ -4,52 +4,45 @@ sys.stdin = open("input.txt")
 T = int(input())
 
 for tc in range(1, T+1):
-    N = int(input())  # N(5 ≤ N ≤ 20)
-
-    omok = []
-    for _ in range(N):
-        omok.append(input())  # 오목판 받기
-
-    N = len(omok)
-    num = 0
+    N = int(input())  # N : 오목판 크기(5 ≤ N ≤ 20)
+    omok = [input() for _ in range(N)]  # 오목판 문자열 받기
+    num = 0  # num : int -- 5개 연속한 부분의 개수
 
     # 가로방향 검사
     for i in range(N):
         if 'ooooo' in omok[i]:
             num += 1
 
-    # 세로방향 검사
-    for j in range(N):
-        for i in range(N):
-            cnt = 0
-            if 0 <= i < N and omok[i][j] == 'o':
-                while 0 <= i < N and omok[i][j] == 'o':
-                    i += 1
-                    cnt += 1
-            if cnt >= 5:
+    # 세로방향 검사 :
+    for i in range(N-4):
+        for j in range(N):
+            for move in range(5):
+                if omok[i + move][j] != 'o':
+                    break
+            else:
                 num += 1
 
-    # 대각방향 검사
-    for i in range(N):
-        cnt = 0
-        if 0 <= i < N and omok[i][i] == 'o':
-            while 0 <= i < N and omok[i][i] == 'o':
-                i += 1
-                cnt += 1
-            if cnt >= 5:
+    # 대각방향 검사 :
+    for i in range(N-4):
+        for j in range(N-4):
+            for move in range(5):
+                if omok[i + move][j + move] != 'o':
+                    break
+            else:
                 num += 1
 
-    # 역대각방향 검사
-    for i in range(N):
-        cnt = 0
-        if 0 <= i < N and omok[i][-1 - i] == 'o':
-            while 0 <= i < N and omok[i][-1 - i] == 'o':
-                i += 1
-                cnt += 1
-            if cnt >= 5:
+    # 역대각방향 검사:
+    for i in range(N-4):
+        for j in range(N-1, 3, -1):
+            for move in range(5):
+                if omok[i + move][j - move] != 'o':
+                    break
+            else:
                 num += 1
 
     if num >= 1:
-        print(f'#{tc} Yes')
-    else: print(f'#{tc} No')
+        print(f'#{tc} YES')
+    else:
+        print(f'#{tc} NO')
+
 
