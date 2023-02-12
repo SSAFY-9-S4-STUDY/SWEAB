fde6dab0b43b617985f8480a7606598d70b00f32
Koneweekk <rlagkswn37@naver.com>
2023-02-13 00:39:04 +0900

    add solution

--- day8/B_2981/B_2981_hanju.py
+++ day8/B_2981/B_2981_hanju.py
@@ -1,15 +1,4 @@
-import sys
-
-def common_divsor(n, m):
-    res = []
-    for i in range(1, int(n**0.5)+2):
-        if n % i == 0:
-            if i > m:
-                res.append(i)
-            if n // i > m:
-                res.append(n // i)
-    return res
-
+# 유클리드 호제법을 이용하여 최대 공약수 구하기
 def gcd(lst, n):
     while n != 1:
         small = min(lst[:2])
@@ -22,42 +11,23 @@ def gcd(lst, n):
             n -= 1
     return lst[0]
 
+import sys
 
 N = int(sys.stdin.readline())
 numbers = sorted([int(sys.stdin.readline()) for _ in range(N)])
-min_value, second_value = numbers[0], numbers[1]
 
-answer = []
-# 두 번째값이 최솟값의 2배보다 크다면 최솟값 이상의 값부터 탐색
-if second_value > min_value * 2:
-    tmp = gcd(list(map(lambda x: x-min_value, numbers[1:])), N-1)
-    if tmp > min_value:
-        answer = common_divsor(tmp,min_value)
+sub_numbers = list(map(lambda x: x-numbers[0],numbers))[1:]
+gcd_num = gcd(sub_numbers, N)
 
+answer = [gcd_num]
+for div in range(2, int(gcd_num**0.5) + 1):
+    if gcd_num % div == 0:
+        answer.append(div)
+        if div != gcd_num**0.5:
+            answer.append(gcd_num // div)
 
-# 최솟값까지 탐색
-m = [0] * (min_value+1)
-for div in range(min_value,1,-1):
-    if not m[div]:
-        mod = min_value % div
-        for i in range(1,N):
-            if numbers[i] % div != mod:
-                break
-        else:
-            for i in common_divsor(div,1):
-                m[i] = 1
+print(*sorted(answer))
 
-
-for i in range(2, min_value+1):
-    if m[i]:
-        answer.append(i)
-
-
-tmp = 0
-for i in sorted(answer):
-    if i != tmp:
-        print(i, end=' ')
-        tmp = i
 
 
 
