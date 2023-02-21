88da9a967392374f60af04e347e3e5ee008b28d9
dogfoothuman <dogfoothuman@gmail.com>
2023-02-21 15:08:52 +0900

    add comment

--- day10/B_1874/B_1874_Ryu.py
+++ day10/B_1874/B_1874_Ryu.py
@@ -20,3 +20,10 @@ if stk:
     print('NO')
 else:
     print('\n'.join(ans))
+
+'''
+sys.stdin.readline()을 사용하지 않으면 시간초과가 나옵니다.
+사용하지 않는다면 stk = []에서 append, pop하는 방법이 아니라
+stk = [0 for _ in range(num)]  top = -1을 활용해야합니다.
+시간초과가 뜨지는 않는데 4000ms 나옵니다...
+'''
