4b35f6bb296e19e9e1db83187e3eae3f63f1eeab
gus991121 <gus991121@naver.com>
2023-03-19 18:19:37 +0900

    jihyun_12100_modi

--- day14/B_12100/B_12100_JIHYUN.py
+++ day14/B_12100/B_12100_JIHYUN.py
@@ -8,6 +8,8 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 # 일단 올리고 집가서 다시해볼게요
 # 신박한 방법을 찾고 싶었는데 그냥 하드코딩 했습니다 키킼
 # 저 답이 계속 틀리네요^^ ㅎㅎㅎ
+# 디버깅 성공했다고 생각했는데 또 틀렸네용 ㅎㅎㅎ
+# 우와 디버깅 성공 ㅠㅠㅠㅠㅠㅠㅠ
 
 
 def move(arr, direction):
@@ -18,9 +20,9 @@ def move(arr, direction):
             combine = True      # True: 합쳐질 수 있다 == 합쳐진 적이 없다
             for i in range(N):
                 if arr[i][j] and fill_index == 0:
-                    arr[fill_index][j] = arr[i][j]
+                    arr[0][j] = arr[i][j]
                     fill_index += 1
-                elif arr[i][j] and fill_index:
+                elif arr[i][j]:
                     if combine and arr[fill_index - 1][j] == arr[i][j]:
                         arr[fill_index - 1][j] *= 2
                         combine = False
@@ -28,7 +30,11 @@ def move(arr, direction):
                         arr[fill_index][j] = arr[i][j]
                         fill_index += 1
                         combine = True
-                arr[i][j] = 0
+            else:   # 행을 다 돌면
+                if fill_index == N:
+                    continue
+                for rest in range(fill_index, N):
+                    arr[rest][j] = 0
 
     # 아래
     elif direction == 1:
@@ -37,9 +43,9 @@ def move(arr, direction):
             combine = True
             for i in range(N-1, -1, -1):
                 if arr[i][j] and fill_index == N - 1:
-                    arr[fill_index][j] = arr[i][j]
+                    arr[N - 1][j] = arr[i][j]
                     fill_index -= 1
-                elif arr[i][j] and fill_index:
+                elif arr[i][j]:
                     if combine and arr[fill_index + 1][j] == arr[i][j]:
                         arr[fill_index + 1][j] *= 2
                         combine = False
@@ -47,7 +53,11 @@ def move(arr, direction):
                         arr[fill_index][j] = arr[i][j]
                         fill_index -= 1
                         combine = True
-                arr[i][j] = 0
+            else:
+                if fill_index == -1:
+                    continue
+                for rest in range(fill_index, -1, -1):
+                    arr[rest][j] = 0
 
     # 왼쪽
     elif direction == 2:
@@ -56,9 +66,9 @@ def move(arr, direction):
             combine = True
             for j in range(N):
                 if arr[i][j] and fill_index == 0:
-                    arr[i][fill_index] = arr[i][j]
+                    arr[i][0]= arr[i][j]
                     fill_index += 1
-                elif arr[i][j] and fill_index:
+                elif arr[i][j]:
                     if combine and arr[i][fill_index - 1] == arr[i][j]:
                         arr[i][fill_index - 1] *= 2
                         combine = False
@@ -66,18 +76,22 @@ def move(arr, direction):
                         arr[i][fill_index] = arr[i][j]
                         fill_index += 1
                         combine = True
-                arr[i][j] = 0
+            else:
+                if fill_index == N:
+                    continue
+                for rest in range(fill_index, N):
+                    arr[i][rest] = 0
 
     # 오른쪽
     else:
         for i in range(N):
-            fill_index = 0
+            fill_index = N - 1
             combine = True
-            for j in (N-1, -1, -1):
+            for j in range(N-1, -1, -1):
                 if arr[i][j] and fill_index == N - 1:
-                    arr[i][fill_index] = arr[i][j]
+                    arr[i][N - 1] = arr[i][j]
                     fill_index -= 1
-                elif arr[i][j] and fill_index:
+                elif arr[i][j]:
                     if combine and arr[i][fill_index + 1] == arr[i][j]:
                         arr[i][fill_index + 1] *= 2
                         combine = False
@@ -85,8 +99,11 @@ def move(arr, direction):
                         arr[i][fill_index] = arr[i][j]
                         fill_index -= 1
                         combine = True
-                arr[i][j] = 0
-
+            else:
+                if fill_index == -1:
+                    continue
+                for rest in range(fill_index, -1, -1):
+                    arr[i][rest] = 0
     return arr
 
 
@@ -99,10 +116,11 @@ def dfs(cnt, arr):
         return
 
     # 5번 아니면 상하좌우 방향으로 또 움직여
-    dfs(cnt + 1, move(deepcopy(arr), 0))  # 0: 위
-    dfs(cnt + 1, move(deepcopy(arr), 1))  # 1: 아래
-    dfs(cnt + 1, move(deepcopy(arr), 2))  # 2: 왼쪽
-    dfs(cnt + 1, move(deepcopy(arr), 3))  # 3: 오른쪽
+    cnt += 1
+    dfs(cnt, move(deepcopy(arr), 0))  # 0: 위
+    dfs(cnt, move(deepcopy(arr), 1))  # 1: 아래
+    dfs(cnt, move(deepcopy(arr), 2))  # 2: 왼쪽
+    dfs(cnt, move(deepcopy(arr), 3))  # 3: 오른쪽
 
 
 maxi = 0
