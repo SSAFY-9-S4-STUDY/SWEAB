f83d24e138e8dafe7b27919e193af6e2b78cd5ca
wony.dev <programmerwony@gmail.com>
2023-03-20 23:22:52 +0900

    B_4803 one more try

--- day15/B_4803/B_4803_Jangwon.py
+++ day15/B_4803/B_4803_Jangwon.py
@@ -1,16 +1,21 @@
-## 뭔가 풀 수 있을 거 같아서 이거는 일단 중간과정만 올리고 다시 올릴게요....ㅜ
+## 뭔가 풀 수 있을 거 같아서 이거는 일단 중간과정만 올리고 다시 올릴게요
 
-def dfs(start, cnt):
+def dfs(end, start):
+    global status
     if not visited[start]:
-        visited[start] = cnt
+        visited[start] = end
 
     for node in range(1, N + 1):
         if arr[start][node]:
+            if node == end:
+                continue
             if visited[node]:
+                status = False
                 return
-            else:
-                dfs(node, cnt + 1)
+            if not visited[node]:
+                dfs(start, node)
 
+tc = 0
 while True:
     N, M = map(int, input().split())
 
@@ -24,3 +29,20 @@ while True:
         for _ in range(M):
             s, e = map(int, input().split())
             arr[s][e] = arr[e][s] = 1
+
+
+        ans = 0
+        for i in range(1, N + 1):
+            if not visited[i]:
+                status = True
+                dfs(i, i)
+                if status:
+                    ans += 1
+
+        tc += 1
+        if ans > 1:
+            print(f'Case {tc}: A forest of {ans} trees.')
+        elif ans == 1:
+            print(f'Case {tc}: There is one tree.')
+        else:
+            print(f'Case {tc}: No trees.')
