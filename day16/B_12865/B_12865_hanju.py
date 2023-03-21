b8b4676e8695f03f06855964c9cc313daa38448e
Koneweekk <rlagkswn37@naver.com>
2023-03-22 08:51:05 +0900

    change code B_12865

--- day16/B_12865/B_12865_hanju.py
+++ day16/B_12865/B_12865_hanju.py
@@ -1,11 +1,12 @@
+# 각 무게마다 최대 가치를 찾고
+# 그 중 최대값을 찾는 로직
+
 N, K = map(int, input().split())
-items = sorted([tuple(map(int, input().split())) for _ in range(N)])
+items = [(map(int, input().split())) for _ in range(N)]
+values = [0]*(K+1)
 
-values, weights= [0]*(K+1), set()
-
-for i in items:
-    for j in range(K,i[0]-1,-1):
-        tmp_v = values[j-i[0]]+i[1]
-        values[j] = max(values[j], tmp_v)
+for w, v in items:
+    for i in range(K, w-1, -1):
+        values[i] = max(values[i], values[i-w] + v)
     
 print(max(values))
