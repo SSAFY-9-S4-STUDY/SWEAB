0f8c3efca08ca13e303dfb27ad2fd29c556b63b8
Sseo Jjam <sjh960815@gmail.com>
2023-04-22 14:44:45 +0900

    commit P_118668_seoJam

--- day21/P_118668/P_118668_seoJam.py
+++ day21/P_118668/P_118668_seoJam.py
@@ -1,41 +1,51 @@
 from pprint import pprint
 
 def solution(alp, cop, problems):
-
+    # [1] 알고력, 코딩력 최댓값 구하기
     max_alp = max_cop = 0
     for problem in problems:
         max_alp = max(max_alp, problem[0])
         max_cop = max(max_cop, problem[1])
+
+    # [2] 최초 알고력, 코딩력이 최댓값 보다 크거나 같으면? 0 반환
+    if alp >= max_alp and cop >= max_cop:
+        return 0
+    # 둘 중 하나라도 최댓값 보다 크거나 같을수도..(thx to 지현)
+    alp = min(alp, max_alp)
+    cop = min(cop, max_cop)
+
+    # [3] DP 만들기
     max_alp -= alp
     max_cop -= cop
-
-    dp = list([100 for j in range(max_cop)] for i in range(max_alp))
+    dp = list([al+co for co in range(max_cop+1)] for al in range(max_alp+1))
     dp[0][0] = 0  # 출발점
-    pprint(dp, width=500)
-    for i in range(max_alp):
-        for j in range(max_cop):
-            # [1] 알파력 1시간 공부
-            dp[i+1][j] = min(dp[i+1][j], dp[i][j] + 1)
-            # [2] 코딩력 1시간 공부
-            dp[i][j+1] = min(dp[i][j+1], dp[i][j] + 1)
-            # [3] 문제 풀기
+
+    # [4] 문제 풀기
+    for i in range(max_alp+1):
+        for j in range(max_cop+1):
             for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
-                nal_req, nco_req = alp_req - alp, cop_req - cop
                 # 문제 풀 수 있다면?
-                if i >= nal_req and j >= nco_req:
+                if i >= alp_req-alp and j >= cop_req-cop:
+                    # 멍청하게 아래처럼 조건 4개씩 달았다가.. 지현님꺼 보고 수정했습니다.
+                    ni, nj = min(i + alp_rwd, max_alp), min(j + cop_rwd, max_cop)
+                    dp[ni][nj] = min(dp[ni][nj], dp[i][j] + cost)
 
-                    if i+alp_rwd > max_alp and j+cop_rwd > max_cop:
-                        dp[max_alp][max_cop] = min(dp[max_alp][max_cop], dp[i][j] + cost)
-                    elif i+alp_rwd > max_alp:
-                        dp[max_alp][j+cop_rwd] = min(dp[max_alp][j+cop_rwd], dp[i][j] + cost)
-                    elif j+cop_rwd > max_cop:
-                        dp[i+alp_rwd][max_cop] = min(dp[i+alp_rwd][max_cop], dp[i][j] + cost)
-                    else:
-                        dp[i+alp_rwd][j+cop_rwd] = min(dp[i+alp_rwd][j+cop_rwd], dp[i][j] + cost)
-
-    pprint(dp)
-    answer = dp[max_alp-1][max_alp-1]
+                    # # (1) 알고력, 코딩력 둘 다 max 값을 넘겼을 때
+                    # if i + alp_rwd > max_alp and j + cop_rwd > max_cop:
+                    #     dp[max_alp][max_cop] = min(dp[max_alp][max_cop], dp[i][j] + cost)
+                    # # (2)알고력만 max 값을 넘겼을 때
+                    # elif i+alp_rwd > max_alp:
+                    #     dp[max_alp][j+cop_rwd] = min(dp[max_alp][j+cop_rwd], dp[i][j]+cost)
+                    # # (3) 코딩력만 max 값을 넘겼을 때
+                    # elif j+cop_rwd > max_cop:
+                    #     dp[i+alp_rwd][max_cop] = min(dp[i+alp_rwd][max_cop], dp[i][j]+cost)
+                    # # (4) 둘 다 max 값보다 작을 때
+                    # else:
+                    #     dp[i+alp_rwd][j+cop_rwd] = min(dp[i+alp_rwd][j+cop_rwd], dp[i][j]+cost)
+    # pprint(dp)
+    answer = dp[max_alp][max_cop]
     return answer
 
 
+print(solution(10, 10, [[10,15,2,1,2],[20,20,3,3,4]]))
 print(solution(0, 0, [[0,0,2,1,2],[4,5,3,1,2],[4,11,4,0,2],[10,4,0,4,2]]))
