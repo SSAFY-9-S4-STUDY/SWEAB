a6d79bc590b1553da3a5ebfd3470090b47e22700
Sseo Jjam <sjh960815@gmail.com>
2023-06-21 22:29:46 +0900

    연ì속펄스부분수열의합 완료

--- day28/P_161988/P_161988_seoJam.py
+++ day28/P_161988/P_161988_seoJam.py
@@ -1,28 +1,17 @@
-# 펄스 수열의 합 중 큰 것 반환
-def sum_pulse(sequence):
-    sum_odd = sum_even = 0
-
-    for idx in range(len(sequence)):
-        if idx % 2:
-            sum_odd += sequence[idx]
-        else:
-            sum_even += sequence[idx]
-
-    return max(sum_odd, sum_even) - min(sum_odd, sum_even)
-
-
 def solution(sequence):
-    answer = -1e9
+    n = len(sequence)
+    pulse1 = list(sequence[idx] if idx % 2 else -sequence[idx] for idx in range(n))  # (-1, 1) 순으로 곱해준 수열
+    pulse2 = list(map(lambda x: -x, pulse1))                                         # (1, -1) 순으로 곱해준 수열
+    dp1 = [pulse1[0]] + [0] * (n - 1)  # pulse1에서 부분 수열의 합
+    dp2 = [pulse2[0]] + [0] * (n - 1)  # pulse2에서 부분 수열의 합
 
-    for idx in range(len(sequence)):
-        temp = list()
+    # [1] 비교: 현재 idx 수열값  vs  현재 idx 까지의 연속된 수열의 합
+    for i in range(1, n):
+        dp1[i] = max(pulse1[i], dp1[i-1] + pulse1[i])
+        dp2[i] = max(pulse2[i], dp2[i-1] + pulse2[i])
 
-        while idx < len(sequence):
-            temp.append(sequence[idx])
-            answer = max(answer, sum_pulse(temp))
-            idx += 1
-
-    return answer
+    # [2] dp1과 dp2의 최댓값 중 더 큰값 반환
+    return max(max(dp1), max(dp2))
 
 
-print(solution([2, 3, -6, 1, 3, -1, 2, 4]))
+# print(solution([2, 3, -6, 1, 3, -1, 2, 4]))
