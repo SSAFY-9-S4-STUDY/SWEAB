843815db24d94314dbf0c57bb5991919dcbd6e85
Koneweekk <rlagkswn37@naver.com>
2023-07-11 17:51:12 +0900

    add day31 folder

---
+++ day31/P_72415/P_72415_hanju.py
@@ -0,0 +1,19 @@
+def solution(board, row, col):
+    # 좌표간 최단 거리 찾기
+    def shortest(r1, c1, r2, c2):
+        visited = [[99]*4 for _ in range(4)]
+        
+    cards_loc = {i:[] for i in range(1, 9)}
+    for r in range(4):
+        for c in range(4):
+
+    def start_card(row, col, oper):
+
+
+
+
+    total, answer = (16-sum(board, []).count(0))//2, 9999
+    search(row, col, 0, 0)
+    return answer
+
+print(solution([[3,0,0,2],[0,0,1,0],[0,1,0,0],[2,0,0,3]], 0, 1))
