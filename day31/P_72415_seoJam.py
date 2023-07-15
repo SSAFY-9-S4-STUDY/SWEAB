9d1f8b4e2bdef4f5ca23b5551370ad8dea99cf6a
Sseo Jjam <sjh960815@gmail.com>
2023-07-15 11:30:26 +0900

    중간 저장

--- day31/P_72415_seoJam.py
+++ day31/P_72415_seoJam.py
@@ -1,84 +1,71 @@
-# from collections import defaultdict
-#
-#
-# def solution(_board, r, c):
-#     answer = 0
-#     global board
-#     board = _board
-#
-#      # [1] 짝 위치 기억해두기
-#     pairs = defaultdict(list)
-#     for i in range(4):
-#         for j in range(4):
-#             if board[i][j]:
-#                 pairs[board[i][j]].append((i, j))
-#
-#     # [2] 이동하기
-#     print(get_cnt((1,0), (2,3), 0))
-#
-#     return answer
-#
-#
-# def is_in(r, c):
-#     if r < 0 or 3 < r or c < 0 or 3 < c:
-#         return False
-#     return True
-#
-#
-# def get_cnt(now, target, cnt):
-#     dirs = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-#     max_cnt = 4
-#
-#     if cnt >= max_cnt:
-#         return
-#
-#     if now == target:
-#         return min(cnt, max_cnt)
-#
-#     r, c = now
-#     for dr, dc in dirs:
-#         get_cnt((r+dr, c+dc), target, cnt+1)
-#         get_cnt(ctrl_move(r, c, dr, dc), target, cnt+1)
-#
-#
-# def ctrl_move(r, c, dr, dc):
-#     while is_in(r+dr, c+dc) and not board[r+dr][c+dc]:
-#         r, c = r + dr, c + dc
-#     return r, c
-
-#
-# def move(r, c, tr, tc):
-#     cnt = 1
-#
-#     if r == tr:
-#         if tc < c:  # 좌
-#
-#         else:       # 우
-#             pass
-#     elif c == tc:
-#         if tr < r:  # 상
-#             pass
-#         else:       # 하
-#             pass
-#     elif tr < r:
-#         if c < tc:  # 1사분면
-#             pass
-#         else:       # 2사분면
-#             pass
-#     else:
-#         if tc < c:  # 3사분면
-#             pass
-#         else:       # 4사분면
-#             pass
-
-
-
-def solution(board, r, c):
+# 최소 노드 경로..
+def solution(_board, r, c):
+    global board, pairs
+    board = _board
     answer = 0
+    print(bfs(r, c))
+
     return answer
 
 
+# board 안에 있는지 판단
+def is_in(r, c):
+    if r < 0 or 3 < r or c < 0 or 3 < c:
+        return False
+    return True
 
 
-print(solution([[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]], 1, 0))
-print(solution([[3,0,0,2],[0,0,1,0],[0,1,0,0],[2,0,0,3]], 0, 1))
+# ctrl + 방향키
+def ctrl_move(r, c, dr, dc):
+    while is_in(r+dr, c+dc) and not board[r+dr][c+dc]:
+        r, c = r + dr, c + dc
+    return r, c
+
+
+# pair 찾기 함수
+def bfs(r, c):
+    q = set()
+    q.add((r, c))
+    distance = [[16 for _ in range(4)] for _ in range(4)]
+    distance[r][c] = 0  # 시작점 표시
+    dirs = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+    target = board[r][c]
+    print('target: ', target)
+
+    while q:
+        r, c = q.pop()
+
+        for dr, dc in dirs:
+            r1, c1 = r + dr, c + dc
+            r2, c2 = ctrl_move(r, c, dr, dc)
+
+            if not is_in(r1, c1) or (r, c) == (r2, c2):
+                continue
+
+            if board[r1][c1] == target:
+                print('도착1: ', board[r1][c1])
+                board[r1][c1] = 0
+                return distance[r][c] + 1
+
+            elif board[r2][c2] == target:
+                print('도착2: ', board[r2][c2])
+                board[r2][c2] = 0
+                return distance[r][c] + 1
+
+            else:
+                if distance[r1][c1] > distance[r][c] + 1:
+                    distance[r1][c1] = distance[r][c] + 1
+                    q.add((r1, c1))
+                if distance[r2][c2] > distance[r][c] + 1:
+                    distance[r2][c2] = distance[r][c] + 1
+                    q.add((r2, c2))
+
+        print(distance)
+
+    board[r][c] = 0
+
+
+solution([[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]], 1, 0)
+
+# print(solution([[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]], 1, 0))
+# print(solution([[3,0,0,2],[0,0,1,0],[0,1,0,0],[2,0,0,3]], 0, 1))
