a5811faabddf0f64ad8fde3bd3018e107652a411
Sseo Jjam <sjh960815@gmail.com>
2023-08-09 20:57:15 +0900

    feat: 서쨈 day34 완료

--- day34/P_214288/P_214288_seoJam.py
+++ day34/P_214288/P_214288_seoJam.py
@@ -1,21 +1,22 @@
 import heapq
-from copy import deepcopy
 
 
 def solution(k, n, reqs):
     answer = 1e9
     temp = list([] for _ in range(k + 1))
-
+    # [1] 상담 유형별로 구분짓기
     for req in reqs:
         *args, category = req
         temp[category].append(req)
-
+    # [2] 상담 유형별 상담원을 분배하는 모든 경우의 수
     combs = get_partitions(k, n)
-
+    # [3] 각 경우마다 대기시간 구하기
     for comb in combs:
         temp_ans = 0
         for i in range(1, k + 1):
             temp_ans += wait_time(comb[i], temp[i])
+            if answer < temp_ans:
+                continue
         answer = min(answer, temp_ans)
 
     return answer
@@ -23,10 +24,10 @@ def solution(k, n, reqs):
 
 def get_partitions(k, n):
     partitions = list(map(lambda x: [0] + x, partition(k, n)))
-
     return partitions
 
 
+# 상담유형별 상담원 배치하는 경우의 수 반환
 def partition(k, n):
     res = []
 
@@ -42,38 +43,19 @@ def partition(k, n):
 
 
 def wait_time(n, reqs):
+    # 상담원이 더 많은 경우
+    if len(reqs) <= n:
+        return 0
+
     res = 0
-    heap = []
-    waitlist = deepcopy(reqs)
+    heap, waitlist = [0] * n, reqs
 
-    for _ in range(min(n, len(waitlist))):
-        s, dt, _ = waitlist.pop(0)
-        heapq.heappush(heap, s + dt)
-
-    while waitlist:
-        s, dt, _ = waitlist.pop(0)
+    for waiter in waitlist:
+        s, dt, _ = waiter
         e = heapq.heappop(heap)
-        wt = e - s if s < e else 0
+        wt = e - s if s < e else 0  # 대기시간
         res += wt
-        heapq.heappush(heap, e + dt)
+        e = e + dt if s < e else s + dt  # 상담종료시각
+        heapq.heappush(heap, e)
 
     return res
-
-
-print(
-    solution(
-        3,
-        5,
-        [
-            [10, 60, 1],
-            [15, 100, 3],
-            [20, 30, 1],
-            [30, 50, 3],
-            [50, 40, 1],
-            [60, 30, 2],
-            [65, 30, 1],
-            [70, 100, 2],
-        ],
-    )
-)
-print(solution(2, 3, [[5, 55, 2], [10, 90, 2], [20, 40, 2], [50, 45, 2], [100, 50, 2]]))
