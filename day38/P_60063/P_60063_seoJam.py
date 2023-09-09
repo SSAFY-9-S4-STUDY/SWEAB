d7ecac06c493911df0f37558c7de59ddb7d9c680
Sseo Jjam <sjh960815@gmail.com>
2023-09-09 17:23:07 +0900

    fix: day38 푸는 중

--- day38/P_60063/P_60063_seoJam.py
+++ day38/P_60063/P_60063_seoJam.py
@@ -2,46 +2,44 @@ from collections import deque
 
 
 def solution(board):
-    MAX_VALUE = 1e9
-    DIRS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     N = len(board)
-
-    # board 주변을 1로 패딩
-    new_board = [[1 for _ in range(N + 1)] for _ in range(N + 1)]
-    for i in range(1, N + 1):
-        for j in range(1, N + 1):
-            new_board[i][j] = board[i - 1][j - 1]
-
-    dp = [[MAX_VALUE for _ in range(N + 1)] for _ in range(N + 1)]
-    dp[1][1] = dp[1][2] = 0
-    queue = deque([(1, 1), (1, 2)])
+    TARGET = (N - 1, N - 1)
+    queue = deque([((0, 0), (0, 1), 0)])
+    visited = set([((0, 0), (0, 1))])
+    r, c = 0, 1
 
     while queue:
-        cr, cc = queue.popleft()
+        rob1, rob2, cnt = queue.popleft()
 
-        for dr, dc in DIRS:
-            nr, nc = cr + dr, cc + dc
-            # 범위 확인
-            if nr < 1 or nc < 1 or N < nr or N < nc:
-                continue
-            # 벽 확인
-            if new_board[nr][nc]:
-                continue
-            # 90도 회전 가능 확인
-            if (not dc and new_board[nr][nc - 1] and new_board[nr][nc + 1]) or (
-                not dr and new_board[nr - 1][nc] and new_board[nr + 1][nc]
-            ):
-                continue
-            
-            new_value = dp[cr][cc] + 1
+        if rob1 == TARGET or rob2 == TARGET:
+            return cnt
 
-            if dp[nr][nc] > new_value:
-                dp[nr][nc] = new_value
-                queue.append((nr, nc))
-    print(dp)
-    answer = dp[N][N]
-
-    return answer
+        # 가로 방향일때
+        if rob1[r] == rob2[r]:
+            for nr, nc in [
+                (rob1[r] - 1, rob1[c]),
+                (rob2[r] - 1, rob2[c]),
+                (rob1[r], rob1[c] - 1),
+                (rob2[r], rob2[c] + 1),
+                (rob1[r] + 1, rob1[c]),
+                (rob2[r] + 1, rob2[c]),
+            ]:
+                if nr < 0 or nc < 0 or N <= nr or N <= nc:
+                    continue
+                if board[nr][nc] == 1:
+                    continue
+                
+        # 세로 방향일때
+        elif rob1[c] == rob2[c]:
+            for nr, nc in [
+                (rob1[r] - 1, rob1[c]),
+                (rob1[r], rob1[c] - 1),
+                (rob1[r], rob1[c] + 1),
+                (rob2[r], rob2[c] - 1),
+                (rob2[r], rob2[c] + 1),
+                (rob2[r] + 1, rob2[c]),
+            ]:
+                pass
 
 
 print(
