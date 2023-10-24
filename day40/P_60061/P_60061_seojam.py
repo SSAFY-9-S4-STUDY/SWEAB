81baa7693d005c4a3bdab914af1b2ead43d8825e
서재현 <sjh960815@naver.com>
2023-10-24 10:39:54 +0900

    서쨈: day40 제출

--- day40/P_60061/P_60061_seojam.py
+++ day40/P_60061/P_60061_seojam.py
@@ -8,28 +8,33 @@ def solution(n, build_frame):
         else: 
             wall.remove([x, y, obj])  # 제거
 
-        flag = False
 
         # 설치 or 제거 후 구조물 상태 확인
-        for [wx, wy, _] in wall:
-            if wx < x - 1 or wx > x + 1 or  wy < y -1 or wy > y + 1:
+        safe = True
+        
+        for [wx, wy, wobj] in wall:
+            # 검사 필요 없는 자재들은 통과 (ref.지현's)
+            if wx < x -1 or x + 1 < wx or wy < y -1 or y + 1 < wy:
                 continue
-            if obj == 0:
-                if y == 0 or [wx - 1, wy, 1] in wall or [wx, wy, 1] in wall or [wx, wy - 1, 0] in wall:
-                    flag= True
-                    break
-            else:
-                if [wx, wy - 1, 0] in wall or [wx + 1, wy - 1, 0] in wall or ([wx - 1, y, 1] in wall and [wx + 1, y, 1] in wall):
-                  flag = True
-                  break
-        print(flag)
-        if flag == False:
-            if build: wall.pop()
-            else: wall.append([x, y, obj])
+            # 기둥 검사
+            if wobj == 0:
+                if wy == 0 or [wx - 1, wy, 1] in wall or [wx, wy, 1] in wall or [wx, wy - 1, 0] in wall:
+                    continue
+                safe = False
+            # 보 검사
+            elif wobj == 1:
+                if [wx, wy - 1, 0] in wall or [wx + 1, wy - 1, 0] in wall or ([wx - 1, wy, 1] in wall and [wx + 1, wy, 1] in wall):
+                  continue
+                safe = False
 
+        if not safe:
+            if build: 
+                wall.remove([x, y, obj])
+            else: 
+                wall.append([x, y, obj])
 
-    return wall.sort()
-
+    return sorted(wall)
 
 
 print(solution(5,	[[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]))
+print(solution(5,	[[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]))
